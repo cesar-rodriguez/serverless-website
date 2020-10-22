434ef190574c794ac1953af85ad0baeab1c46316
Cesar Rodriguez <cesar@accurics.com>
2020-10-22 00:29:18 -0400

    adds bucket policy

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -25,21 +25,16 @@ resource "aws_s3_bucket_policy" "b" {
     "Version": "2012-10-17",
     "Statement": [
         {
-            "Sid": "GetObject",
+            "Sid": "Object",
             "Effect": "Deny",
             "Principal": "*",
             "Action": [
-                "s3:GetObject"
+                "s3:PutBucketWebsite"
             ],
             "Resource": [
                 "arn:aws:s3:::${var.bucket_name}",
                 "arn:aws:s3:::${var.bucket_name}/*"
-            ],
-            "Condition" : {
-                "StringNotEquals" : {
-                    "aws:SourceVpc": "${var.vpc}"
-                }
-            }
+            ]
         }
     ]
 }
