bfcf9a0475fe4c1444c73eaade20a446146e2b19
Cesar Rodriguez <cesar@accurics.com>
2020-10-22 00:32:54 -0400

    adds all vars

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,11 @@
 ### Variables
 variable "aws_region" {}
+variable "s3_staging_bucket" {}
+variable "lambda_version" {}
+variable "name" {}
+variable "subnet" {}
+variable "vpc" {}
+variable "security_group" {}
 
 ### AWS Provider
 provider "aws" {
